c085b932c0540591bcc4f5ff0565f1633f4a50c1
Vincent Rehm <vincent.rehm@ifsalpha.com>
2020-07-27 19:13:18 +0200

    rename variables from a forked project

--- application/config/database.js
+++ application/config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
                 client: 'postgres',
                 host: env('DB_HOST', '127.0.0.1'),
                 port: env.int('DB_PORT', 5432),
-                database: env('DATABASE_NAME', 'colivme'),
+                database: env('DATABASE_NAME', 'tilia'),
                 username: env('DATABASE_USERNAME', 'postgres'),
                 password: env('DATABASE_PASSWORD'),
                 ssl: env.bool('DATABASE_SSL', false),
