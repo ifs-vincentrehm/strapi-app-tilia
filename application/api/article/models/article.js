d2b55a1ff4816cebc53b6f341b8f2c5bb5f6b1ac
Vincent Rehm <vincent.rehm@ifsalpha.com>
2020-07-28 17:49:49 +0200

    add author and upload cdn

--- application/api/article/models/article.js
+++ application/api/article/models/article.js
@@ -13,10 +13,18 @@ module.exports = {
           if (data.title) {
             data.slug = slugify(data.title, {lower: true});
           }
+
+          if (data.published_at) {
+            data.published_at = new Date();
+          }
         },
         async beforeUpdate(params, data) {
           if (data.title) {
             data.slug = slugify(data.title, {lower: true});
+          }
+
+          if (data.published_at === null) {
+            data.published_at = new Date();
           }
         },
       }
